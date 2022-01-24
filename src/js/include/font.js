605c4aa684b63fba5f61babf0c782efb5d51f214
hokuto-kato <hokuto15@gmail.com>
2022-01-24 10:33:20 +0900

    adobe fontの記述やコメント調整

--- src/js/include/font.js
+++ src/js/include/font.js
@@ -1,38 +1,18 @@
 import $ from "jquery"
 export default function () {
 	// adobe font
-	((d) => {
-		let config = {
-				kitId: "tlm4ljk",
+	(function(d) {
+		var config = {
+				kitId: 'tlm4ljk',
 				scriptTimeout: 3000,
-				async: true,
+				async: true
 			},
-			h = d.documentElement,
-			t = setTimeout(() => {
-				h.className =
-					h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive"
-			}, config.scriptTimeout),
-			tk = d.createElement("script"),
-			f = false,
-			s = d.getElementsByTagName("script")[0],
-			a
-		h.className += " wf-loading"
-		tk.src = "https://use.typekit.net/" + config.kitId + ".js"
-		tk.async = true
-		tk.onload = tk.onreadystatechange = function () {
-			a = this.readyState
-			if (f || (a && a !== "complete" && a !== "loaded")) return
-			f = true
-			clearTimeout(t)
-			try {
-				Typekit.load(config)
-			} catch (e) {}
-		}
-		s.parentNode.insertBefore(tk, s)
-	})(document)
+			h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!=="complete"&&a!=="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+	})(document);
 
-	// adobe font ロード完了後にページを表示する設定のため
-	// 低速回線用に3秒後にページを表示させるためのclassをhtmlタグに付与
+	// adobe font ロード完了後にページを表示する設定。
+	// 低速回線でロードが完了しない場合に
+	// 3秒後に強制的にページを表示させるためのclassをhtmlタグに付与
 	setTimeout(() => {
 		$("html").addClass("loading-delay")
 	}, 3000)
