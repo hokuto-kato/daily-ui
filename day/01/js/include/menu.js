fd71ec9486b0b0dc8125a748cd44c56c159dc555
hokuto-kato <hokuto15@gmail.com>
2022-02-05 05:37:33 +0900

    非jQuery化

--- day/01/js/include/menu.js
+++ day/01/js/include/menu.js
@@ -1,31 +1,31 @@
-import $ from "jquery"
-
 export default function() {
-	const $trigger = $("[data-trigger-menu]")
-	const $targetMenu = $("[data-target-menu]")
-	const $targetNav = $("[data-target-nav]")
-	const $targetMenuChild = $("[data-target-menu-child]")
-
-	$trigger.on("click", () => {
-		$trigger.addClass("pointer-events-none")
-		if ($targetMenu.hasClass("active")) {
+	const trigger = document.querySelector("[data-trigger-menu]")
+	const targetMenu = document.querySelector("[data-target-menu]")
+	const targetNav = document.querySelector("[data-target-nav]")
+	const targetMenuChild = document.querySelector("[data-target-menu-child]")
+	trigger.addEventListener("click", () => {
+		trigger.classList.add("pointer-events-none")
+		if (targetMenu.classList.contains("active")) {
 			//非アクティブ
-			$targetMenu.removeClass("active").addClass("close")
-			$targetNav.add($targetMenuChild).removeClass("active")
-			$targetMenu.one("animationend", () => {
-				$targetMenu.add($targetNav).removeClass("close")
-				$targetNav.addClass("visually-hidden")
-				$trigger.removeClass("pointer-events-none")
-			})
+			targetMenu.classList.remove("active")
+			targetMenu.classList.add("close")
+			targetNav.classList.remove("active")
+			targetMenuChild.classList.remove("active")
+			targetMenu.addEventListener("animationend", () => {
+				targetMenu.classList.remove("close")
+				targetNav.classList.remove("close")
+				targetNav.classList.add("visually-hidden")
+				trigger.classList.remove("pointer-events-none")
+			}, { once: true })
 		} else {
 			//アクティブ
-			$targetNav.removeClass("visually-hidden")
-			$targetMenu.add($targetMenuChild)
-				.add($targetNav)
-				.addClass("active")
-			$targetMenu.one("animationend",()=>{
-				$trigger.removeClass("pointer-events-none")
-			})
+			targetNav.classList.remove("visually-hidden")
+			targetMenu.classList.add("active")
+			targetMenuChild.classList.add("active")
+			targetNav.classList.add("active")
+			targetMenu.addEventListener("animationend", () => {
+				trigger.classList.remove("pointer-events-none")
+			}, { once: true })
 		}
 	})
 }
