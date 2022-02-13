7f4077e5749c972fe71e59ad1827b8a037a44f92
hokuto-kato <hokuto15@gmail.com>
2022-02-13 11:14:05 +0900

    day4:webp対応

--- day/04/js/app.js
+++ day/04/js/app.js
@@ -1,8 +1,12 @@
 import sass from "../sass/app.sass"
 import font from "./include/font"
 import menu from "./include/menu"
+import supportWebp from "./include/supportWebp"
 
 document.addEventListener("DOMContentLoaded", () => {
   font()
   menu()
+})
+window.addEventListener("load", () => {
+  supportWebp()
 })
